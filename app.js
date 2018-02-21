18778a071feec919a216f7509a9a6ea95ea2e050
abcbadq <abcbadq@gmail.com>
2018-02-21 00:47:29 -0800

    Removed Test command
    Test command is deprecated and isn't available in commands anymore

--- app.js
+++ app.js
@@ -1,42 +1,33 @@
 const discord = require('discord.js')
 const path = require('path')
 const config = require('./config.json')
-const resources = require('./commands/resources.json')
-const logger = require('bunyan').createLogger({name:'soup'})
+const logger = require('bunyan').createLogger({ name: 'soup' })
 
 var client = new discord.Client({
-  disableEveryone:true,
-  disableEvents:["TYPING_START"]
+  disableEveryone: true,
+  disableEvents: ["TYPING_START"]
 })
 
-process.on('exit',() => {
+process.on('exit', () => {
   client.destroy()
 })
 
-client.on('ready',() => {
+client.on('ready', () => {
   logger.info("Start up complete")
 })
 
 client.on('message', message => {
-  if(message.author.bot || !(message.content.startsWith('!'))){
+  if (message.author.bot || !(message.content.startsWith('!'))) {
     return
   }
   const args = message.content.slice(1).trim().split(/ +/g)
   const command = path.win32.basename(args.shift().toLowerCase())
-  if(args[0] == 'test'){
-    try{
-      require(`./commands/${command}.js`).run(client,message,[resources.testimage],logger)
-    }catch(err){
-      logger.info(err)
-    }
+  try {
+    require(`./commands/${command}.js`).run(client, message, args, logger)
+  } catch (err) {
+    logger.info(err)
   }
-  else{
-    try{
-      require(`./commands/${command}.js`).run(client,message,args,logger)
-    }catch(err){
-      logger.info(err)
-    }
-  }
+
 })
 //token
 client.login(config.token)
